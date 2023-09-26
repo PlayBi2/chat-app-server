31289368c5b2fd69e6416b1cb6e4e9ef4dd203b8
NegativeLearning <PlayBi2002@gmail.com>
2023-09-27 02:10:59 +0700

    socket.io

--- index.js
+++ index.js
@@ -1,23 +1,33 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
-require('dotenv').config()
+require("dotenv").config();
 
-app.use(express.urlencoded({extended: false}))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-const postsRouter = require('./routes/posts.router')
-const authRouter = require('./routes/auth.router')
-const roomRouter = require('./routes/room.router')
-const messageRouter = require('./routes/message.router')
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
 
-app.use("/api/v1/posts", postsRouter)
-app.use("/api/v1/auth", authRouter)
-app.use('/api/v1/rooms',roomRouter)
-app.use('/api/v1/messages',messageRouter)
+const io = new Server(server);
 
-const PORT = process.env.PORT || 3000
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+const postsRouter = require("./routes/posts.router");
+const authRouter = require("./routes/auth.router");
+const roomRouter = require("./routes/room.router");
+const messageRouter = require("./routes/message.router");
+
+app.use("/api/v1/posts", postsRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/rooms", roomRouter);
+app.use("/api/v1/messages", messageRouter);
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("Server is running....")
-})
+  console.log("Server is running....");
+});
